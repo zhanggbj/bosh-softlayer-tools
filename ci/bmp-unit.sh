8bf420e8fa8c66df3c88d693d1e71cf49c2d37dd
Gong Zhang <zhanggbj@cn.ibm.com>
2016-05-20 16:59:30 +0800

    Debug

--- ci/bmp-unit.sh
+++ ci/bmp-unit.sh
@@ -3,13 +3,20 @@
 set -e
 
 go version
+echo "GOPATH is " + &GOPATH
 go get github.com/tools/godep
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/golang/go/src/cmd/vet
 go get github.com/onsi/gomega
 
-echo $PWD
+echo "PWD is " + $PWD
+
+echo "GOPATH is " + &GOPATH
 
 echo "run ut"
 
-./gopath/src/github.com/zhanggbj/bosh-softlayer-tools/bin/test-unit
+sudo find / -name test-unit
+
+gopath/src/github.com/zhanggbj/bosh-softlayer-tools/bin/test-unit
+
+
