31b68d28386f5d48fee17f87ca0236ae482ea882
Gong Zhang <zhanggbj@cn.ibm.com>
2016-05-21 16:02:26 +0800

    debug

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-set -e +x
-base=$( cd "$( dirname "$( dirname "$0" )")" && pwd )
-base_gopath=$( cd $base/../../../.. && pwd )
-export GOPATH=$base/Godeps/_workspace:$base_gopath:$GOPATH
