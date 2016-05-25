3a9b06718544873fa6022b6711b785925f44ce51
Gong Zhang <zhanggbj@cn.ibm.com>
2016-05-25 11:39:00 +0800

    IT start

--- integration/bmp/bms_cmd_test.go
+++ integration/bmp/bms_cmd_test.go
@@ -21,7 +21,7 @@ var _ = Describe("`$bmp bms --deployment` integration tests", func() {
 
 	Describe("$bmp bms --deployment", func() {
 		BeforeEach(func() {
-			deployment, _:= GetDeployment()
+			deployment, _ := GetDeployment()
 			session = RunBmp("bms", "--deployment", deployment)
 		})
 
