dde4b2a9f8eadde9267b54e944dc05646299ab53
Gong Zhang <zhanggbj@cn.ibm.com>
2016-05-25 11:32:12 +0800

    UT pass

--- ci/bmp-integration.sh
+++ ci/bmp-integration.sh
@@ -12,18 +12,16 @@ export GOPATH=$base/Godeps/_workspace:$base_gopath:$GOPATH
 
 echo "GOPATH=" $GOPATH
 
-
 function printStatus {
-      if [ $? -eq 0 ]; then
-          echo -e "\nSWEET SUITE SUCCESS"
-      else
-          echo -e "\nSUITE FAILURE"
-      fi
-  }
+    if [ $? -eq 0 ]; then
+        echo -e "\nSWEET SUITE SUCCESS"
+    else
+        echo -e "\nSUITE FAILURE"
+    fi
+}
 
 trap printStatus EXIT
-
-export BMP_UT_OUTPUT=False
+export GOPATH=$(godep path):$GOPATH
 
 echo -e "\n Cleaning build artifacts..."
 go clean
@@ -34,10 +32,8 @@ go fmt ./...
 echo -e "\n cd to base of project..."
 cd $base
 
-echo -e "\n Unit Testing packages:"
-ginkgo -r -p --noisyPendings --skipPackage=integration
+echo -e "\n Integration Testing packages:"
+ginkgo -r -p -v --noisyPendings integration
 
 echo -e "\n Vetting packages for potential issues..."
-go tool vet main config cmds common integration
-
-
+go tool vet main common cmds integration
