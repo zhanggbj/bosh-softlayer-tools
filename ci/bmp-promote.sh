9f728c5a064e6a8cf8bcaec13bb6602beb6b064c
Gong Zhang <zhanggbj@cn.ibm.com>
2016-05-27 16:32:13 +0800

    promote

--- ci/bmp-promote.sh
+++ ci/bmp-promote.sh
@@ -2,16 +2,8 @@
 
 set -e -x
 
-#source bosh-cpi-release/ci/tasks/utils.sh
-#
-#check_param S3_ACCESS_KEY_ID
-#check_param S3_SECRET_ACCESS_KEY
-#
-#source /etc/profile.d/chruby.sh
-#chruby 2.1.2
-
-# Creates an integer version number from the semantic version format
-# May be changed when we decide to fully use semantic versions for releases
+version=`cat version-semver/number`
+echo $version > promoted/version
 
 base=$( cd "$( dirname "$( dirname "$0" )")" && pwd )
 
@@ -21,16 +13,9 @@ export GOPATH=$base/Godeps/_workspace:$base_gopath:$GOPATH
 
 echo "GOPATH=" $GOPATH
 
-cd $base
-
-version=`cat version-semver/number`
-echo $version > promoted/version
-
-cp -r bosh-softlayer-toos promoted/repo
-
 echo -e "\nGenerating Binary: bmp..."
-go build -o out/bmp-$version main/bmp/bmp.go
-chmod +x out/bmp-$version
+go build -o $base/out/bmp-$version $base/main/bmp/bmp.go
+chmod +x $base/out/bmp-$version
 
-mv out/bmp-$version promoted/
+mv $base/out/bmp-$version promoted/
 
