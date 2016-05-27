de167bb33ce95fa854625a46d652b39c6c75d64b
Gong Zhang <zhanggbj@cn.ibm.com>
2016-05-27 18:37:57 +0800

    final debug

--- ci/tasks/bst-promote.sh
+++ ci/tasks/bst-promote.sh
@@ -23,7 +23,7 @@ chmod +x $base/out/bmp
 
 echo -e "\nGenerating Binary: sl_stemcells..."
 go build -o $base/out/sl_stemcells $base/main/stemcells/stemcells.go
-chmod +x out/sl_stemcells
+chmod +x $base/out/sl_stemcells
 
 echo -e "\nGenerating bosh-softlayer-tools"
 tar -cvf bosh-softlayer-tools-$version.tar -C $base/out/ *
