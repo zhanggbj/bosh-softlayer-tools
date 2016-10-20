27d8d60a8acf144289dc212a1fe9c8a0978e7512
Gong Zhang <zhanggbj@cn.ibm.com>
2016-10-20 10:59:21 +0800

    debug expect

--- ci/tasks/upgrade-stemcell.sh
+++ ci/tasks/upgrade-stemcell.sh
@@ -25,26 +25,27 @@ export BOSH_CLIENT_SECRET=fake_secret
 echo "list vms..."
 bosh vms
 
-ls -al
-
-echo "upload new stemcell..."
-bosh upload stemcell ./stemcell/light-bosh-stemcell-*.tgz
-
 echo "list stemcells"
 bosh stemcells
 
 echo "update stemcell version..."
 bosh_client=${BOSH_CLIENT}
 bosh_client_password=${BOSH_CLIENT_PASSWORD}
-old_stemcell_version=`bosh stemcells|grep light-bosh-stemcell|awk '{print $6}'`
+old_stemcell_version=`bosh stemcells|grep bosh-softlayer-xen-ubuntu-trusty-go_agent|awk '{print $6}'|sed 's/\*//g'`
 echo "DEBUG:old_stemcell_version="$old_stemcell_version
-old_security_version=`bosh releases|grep security-release| awk '{print $4}'`
-echo "DEBUG:old_security_version="$old_security_version
+
+echo "upload new stemcell..."
+bosh upload stemcell ./stemcell/light-bosh-stemcell-*.tgz
 new_stemcell_version=`ls ./stemcell|cut -d "-" -f 3`
 echo "DEBUG:new_stemcell_version="$new_stemcell_version
+
+old_security_version=`bosh releases|grep security-release| awk '{print $4}'`
+echo "DEBUG:old_security_version="$old_security_version
 new_security_version=`curl http://10.106.192.96/releases/security-release/|tail -n 3|head -n 1|cut -d '"' -f 2|sed 's/\///g'`
 echo "DEBUG:new_security_version="$new_security_version
 
+sudo apt-get -y install expect
+set timeout 30
 /usr/bin/env expect<<EOF
 spawn ssh -o StrictHostKeyChecking=no root@bosh_client
 expect "*?assword:*"
