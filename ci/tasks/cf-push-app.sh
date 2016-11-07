16b6c141126168c70e36e662b2fb40ff96c77266
Gong Zhang <zhanggbj@cn.ibm.com>
2016-11-07 14:21:48 +0800

    debug cf push

--- ci/tasks/cf-push-app.sh
+++ ci/tasks/cf-push-app.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 sed -i '1 i\nameserver 10.113.205.104' /etc/resolv.conf
+CF_TRACE=true cf api http://api.gubinpreyf3.ng.bluemix.net
+CF_TRACE=true cf login -u ibmadmin@us.ibm.com -p Passw0rd
+cf push IICVisit -p ./ci/tasks/IICVisit.war
+curl iicvisit.gubinpreyf3.mybluemix.net/GetEnv
