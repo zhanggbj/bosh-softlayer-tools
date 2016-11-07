5a962d51fb2a4dc27e2cbe55c652006b13e09cb7
Gong Zhang <zhanggbj@cn.ibm.com>
2016-11-07 16:30:12 +0800

    add dir

--- ci/tasks/cf-push-app.sh
+++ ci/tasks/cf-push-app.sh
@@ -8,5 +8,6 @@ cf --version
 sed -i '1 i\nameserver 10.113.205.104' /etc/resolv.conf
 CF_TRACE=true cf api http://api.gubinpreyf3.ng.bluemix.net
 CF_TRACE=true cf login -u ibmadmin@us.ibm.com -p Passw0rd
-cf push IICVisit -p ./ci/tasks/IICVisit.war
+base=`dirname "$0"`
+cf push IICVisit -p ${base}/ci/tasks/IICVisit.war
 curl iicvisit.gubinpreyf3.mybluemix.net/GetEnv
