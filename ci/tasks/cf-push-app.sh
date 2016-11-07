9f237b7a1565dab8543a869d301277e45db60d40
Gong Zhang <zhanggbj@cn.ibm.com>
2016-11-07 15:34:38 +0800

    add cf

--- ci/tasks/cf-push-app.sh
+++ ci/tasks/cf-push-app.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+set -e -x
+curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
+mv cf /usr/local/bin
+echo "cf version..."
+cf --version
+
 sed -i '1 i\nameserver 10.113.205.104' /etc/resolv.conf
 CF_TRACE=true cf api http://api.gubinpreyf3.ng.bluemix.net
 CF_TRACE=true cf login -u ibmadmin@us.ibm.com -p Passw0rd
