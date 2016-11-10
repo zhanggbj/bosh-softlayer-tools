c6603efbf4886497fd8fb67b71f2a5591c00f860
Gong Zhang <zhanggbj@cn.ibm.com>
2016-11-10 14:20:33 +0800

    scripts

--- ci/tasks/cf-push-app.sh
+++ ci/tasks/cf-push-app.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
-set -e -x
+set -e
 
 dir=`dirname "$0"`
 source ${dir}/utils.sh
 
-print_title INSTALL CF CLI...
+print_title "INSTALL CF CLI..."
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 mv cf /usr/local/bin
 echo "cf version..."
 cf --version
 
-print_title CF PUSH APP...
+print_title "CF PUSH APP..."
 sed -i '1 i\nameserver 10.113.205.104' /etc/resolv.conf
-CF_TRACE=true cf api http://api.gubinpreyf3.ng.bluemix.net
-CF_TRACE=true cf login -u ibmadmin@us.ibm.com -p Passw0rd
+CF_TRACE=true cf api ${CF-API}
+CF_TRACE=true cf login -u ${CF-USERNAME} -p ${CF-PASSWORD}
 base=`dirname "$0"`
 cf push IICVisit -p ${base}/IICVisit.war
-curl iicvisit.gubinpreyf3.mybluemix.net/GetEnv|grep "DEA IP"
+curl iicvisit.${APP-API}/GetEnv|grep "DEA IP"
 if [ $? -eq 0 ]; then
    echo "cf push app successful!"
+else
+   echo "cf push app failed!"
 fi
--- ci/tasks/verify-security-release.sh
+++ ci/tasks/verify-security-release.sh
@@ -4,62 +4,65 @@ set -e
 dir=`dirname "$0"`
 source ${dir}/utils.sh
 
+print_title "PREPARE SCRIPTS..."
 bosh_cli=${BOSH_CLI}
 bosh_cli_password=${BOSH_CLI_PASSWORD}
 echo "copy scripts..."
-#scripts="run.sh,run.user.expect,test-component.sh"
-#sudo apt-get -y install expect
-#set timeout 10
-#/usr/bin/env expect<<EOF
-#spawn scp -o StrictHostKeyChecking=no root@$bosh_cli:/root/security/\{${scripts}\} ./
-#expect "*?assword:*"
-#exp_send "$bosh_cli_password\r"
-#expect eof
-#EOF
-#ls ./
-#
-#gem install bosh_cli --no-ri --no-rdo c
-#
-#echo "using bosh CLI version..."
-#bosh version
-#
-#echo "login director..."
-#bosh -n target ${BLUEMIX_DIRECTOR_IP}
-#bosh login admin admin
-#
-#export BOSH_CLIENT=fake_client
-#export BOSH_CLIENT_SECRET=fake_secret
-#
-#security_release_version=`curl http://10.106.192.96/releases/security-release/|tail -n 3|head -n 1|cut -d '"' -f 2|sed 's/\///g'`
-#echo "DEBUG security_release_version is"${security_release_version}
-##old_security_version=`bosh releases|grep security-release| awk '{print $4}'|sed 's/\*//g'`
-##echo "DEBUG:old_security_version="$old_security_version
-#echo "verify security release version..."
-#bosh deployments | grep security-release/${security_release_version}
-#if [ $? -ne 0 ]; then
-#  echo "security release version is not correct"
-#  exit 1
-#fi
-#
-#
-#echo "collect VM ip addresses..."
-#bosh vms|awk '/running/{print $11}' > ipaddr.csv
-#run_log="run.log"
-#echo "run test-component.sh on all VMs..."
-#./run.sh -s test-component.sh -i ipaddr.csv -p Paa54futur3 -a | tee $run_log
-#sleep 3
-#
-#cat $run_log | grep "Error connecting to server"
-#if [ $? -eq 0 ]; then
-#   exit 1
-#fi
-#
-#final_result=`awk '/secmon is/{nr[NR+1]}; NR in nr'  $run_log |awk '{ SUM += $1} END { print SUM }'`
-#if [ $final_result -eq 0 ]; then
-#  echo "Security Release Verification Pass..."
-#  exit 0
-#else
-#  echo "Security Release Verification Fail..."
-#  cat $run_log
-#  exit 1
-#fi
+scripts="run.sh,run.user.expect,test-component.sh"
+sudo apt-get -y install expect
+set timeout 10
+/usr/bin/env expect<<EOF
+spawn scp -o StrictHostKeyChecking=no root@$bosh_cli:/root/security/\{${scripts}\} ./
+expect "*?assword:*"
+exp_send "$bosh_cli_password\r"
+expect eof
+EOF
+ls ./
+
+print_title "INSTALL BOSH CLI..."
+gem install bosh_cli --no-ri --no-rdo c
+
+echo "using bosh CLI version..."
+bosh version
+
+echo "login director..."
+bosh -n target ${BLUEMIX_DIRECTOR_IP}
+bosh login admin admin
+
+export BOSH_CLIENT=fake_client
+export BOSH_CLIENT_SECRET=fake_secret
+
+print_title "VERIFY SECURITY RELEASE..."
+security_release_version=`curl http://10.106.192.96/releases/security-release/|tail -n 3|head -n 1|cut -d '"' -f 2|sed 's/\///g'`
+echo "DEBUG security_release_version is"${security_release_version}
+
+echo "verify security release version..."
+bosh deployments | grep security-release/${security_release_version}
+if [ $? -ne 0 ]; then
+  echo "security release version is not correct"
+  exit 1
+fi
+
+echo "collect all VM ip addresses..."
+bosh vms|awk '/running/{print $11}' > ipaddr.csv
+run_log="run.log"
+echo "run test-component.sh on all VMs..."
+./run.sh -s test-component.sh -i ipaddr.csv -p Paa54futur3 -a | tee $run_log
+sleep 3
+
+cat $run_log | grep "Error connecting to server"
+if [ $? -eq 0 ]; then
+   exit 1
+fi
+
+final_result=`awk '/secmon is/{nr[NR+1]}; NR in nr'  $run_log |awk '{ SUM += $1} END { print SUM }'`
+if [ $final_result -eq 0 ]; then
+  echo "Security release verification pass..."
+  exit 0
+else
+  echo "Security release verification fail..."
+  exit 1
+fi
+
+print_title "SECURITY RELEASE VERIFICATION DETAILs..."
+cat $run_log
