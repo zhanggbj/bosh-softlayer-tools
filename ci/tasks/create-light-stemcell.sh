444ae3f068ab3a7405d5b846a858f4c0c6033b90
Jian Qiu Wang <66376385@qq.com>
2017-01-24 16:43:37 +0800

    Add back upload sha1 to bosh.io

--- ci/tasks/create-light-stemcell.sh
+++ ci/tasks/create-light-stemcell.sh
@@ -27,3 +27,10 @@
 
   checksum="$(sha1sum "${base_gopath}/../${output_dir}/${stemcell_filename}" | awk '{print $1}')"
   echo "$stemcell_filename sha1=$checksum"
+  if [ -n "${BOSHIO_TOKEN}" ]; then
+    curl -X POST \
+        --fail \
+        -d "sha1=${checksum}" \
+        -H "Authorization: bearer ${BOSHIO_TOKEN}" \
+        "https://bosh.io/checksums/${stemcell_filename}"
+  fi
